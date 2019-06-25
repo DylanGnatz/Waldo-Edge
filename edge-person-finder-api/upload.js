e8767556544a475df4899869fe57c53abb00d159
Dylan Gnatz <gnatzd@outlook.com>
2019-06-25 16:05:50 -0700

    began adding API endpoints

---
+++ edge-person-finder-api/upload.js
@@ -0,0 +1,30 @@
+import uuid from "uuid";
+import { success, failure } from "./libs/response-lib";
+import * as dynamoDbLib from "./libs/dynamodb-lib";
+import AWS from "aws-sdk";
+
+const dynamoDb = new AWS.DynamoDB.DocumentClient();
+
+export async function main(event, context, callback) {
+  // Request body is passed in as a JSON encoded string in 'event.body'
+  const data = JSON.parse(event.body);
+
+  const params = {
+    TableName: "people",
+    Item: {
+      userId: event.requestContext.identity.cognitoIdentityId,
+      personID: uuid.v1(),
+      personName: data.name,
+      attachment: data.attachment,
+      createdAt: Date.now()
+    }
+  };
+
+  try {
+    await dynamoDbLib.call("put", params);
+    return success(params.Item);
+  } catch (e) {
+    console.log(e);
+    return failure({ status: false });
+  }
+}
