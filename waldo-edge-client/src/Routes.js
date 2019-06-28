090582f2b5fbce35d39e1ef3a763199ccef0b60f
Dylan Gnatz <gnatzd@outlook.com>
2019-06-28 09:14:20 -0700

    added sign out, create form

--- waldo-edge-client/src/Routes.js
+++ waldo-edge-client/src/Routes.js
@@ -3,11 +3,22 @@ import { Route, Switch } from "react-router-dom";
 import Home from "./containers/Home";
 import NotFound from "./containers/NotFound";
 import Login from "./containers/Login";
+import AppliedRoute from "./components/AppliedRoute";
+import Signup from "./containers/Signup";
+import NewPerson from "./containers/NewPerson";
 
-export default () => (
+export default ({ childProps }) => (
   <Switch>
-    <Route path="/" exact component={Home} />
-    <Route path="/login" exact component={Login} />
+    <AppliedRoute path="/" exact component={Home} props={childProps} />
+    <AppliedRoute path="/login" exact component={Login} props={childProps} />
+    <AppliedRoute path="/signup" exact component={Signup} props={childProps} />
+    <AppliedRoute
+      path="/person/new"
+      exact
+      component={NewPerson}
+      props={childProps}
+    />
+    {/* Finally, catch all unmatched routes */}
     <Route component={NotFound} />
   </Switch>
 );
