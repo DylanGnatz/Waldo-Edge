753c6fbbf49f1d82d756fb33107b5dc3dd680f20
Dylan Gnatz <gnatzd@outlook.com>
2019-07-02 10:24:57 -0700

    modified upload to include personID in filename

--- waldo-edge-client/src/libs/awsLib.js
+++ waldo-edge-client/src/libs/awsLib.js
@@ -1,7 +1,7 @@
 import { Storage } from "aws-amplify";
 
-export async function s3Upload(file) {
-  const filename = `${Date.now()}-${file.name}`;
+export async function s3Upload(file, personID) {
+  const filename = `${personID}-${file.name}`;
 
   const stored = await Storage.vault.put(filename, file, {
     contentType: file.type
