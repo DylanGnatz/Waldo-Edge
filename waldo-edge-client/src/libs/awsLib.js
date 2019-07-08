56063a9629c3c1a869bf71877d5559e9fb141976
Dylan Gnatz <gnatzd@outlook.com>
2019-07-08 16:21:05 -0700

    updated upload function to include person name in file name

--- waldo-edge-client/src/libs/awsLib.js
+++ waldo-edge-client/src/libs/awsLib.js
@@ -1,7 +1,8 @@
 import { Storage } from "aws-amplify";
 
-export async function s3Upload(file, personID) {
-  const filename = `${personID}-${file.name}`;
+export async function s3Upload(file, personID, personName) {
+  personName = personName.replace(" ", "_");
+  const filename = `${personID}-${personName}-${file.name}`;
 
   const stored = await Storage.vault.put(filename, file, {
     contentType: file.type
