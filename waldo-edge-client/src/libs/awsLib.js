17db7392ffac0c0a7dd53951bb8008c3b6713ceb
Dylan Gnatz <gnatzd@outlook.com>
2019-07-31 14:08:27 -0700

    updated upload function toappend phone number to file name

--- waldo-edge-client/src/libs/awsLib.js
+++ waldo-edge-client/src/libs/awsLib.js
@@ -1,8 +1,8 @@
 import { Storage } from "aws-amplify";
 
-export async function s3Upload(file, personID, personName) {
+export async function s3Upload(file, personID, personName, phoneNumber) {
   personName = personName.replace(" ", "_");
-  const filename = `${personID}-${personName}-${file.name}`;
+  const filename = `${personID}_${personName}_${phoneNumber}`;
 
   const stored = await Storage.vault.put(filename, file, {
     contentType: file.type
